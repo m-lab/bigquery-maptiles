3ad5def80052440e0e8df0bb297f8c385eceb609
Chris Ritzo <critzo@measurementlab.net>
2020-03-26 17:42:24 -0400

    push all current local work

--- queries/us_counties.sql
+++ queries/us_counties.sql
@@ -10,12 +10,9 @@ WITH counties AS (
 mlab_dl AS (
   SELECT
     tests.*,
-    EXTRACT(YEAR FROM test_date) AS year,
-    EXTRACT(MONTH FROM test_date) AS month,
-    EXTRACT(WEEK FROM test_date) AS week,
-    counties.geo_id AS geo_id,
-    counties.name AS county_name,
-    counties.WKT AS WKT
+    CONCAT(EXTRACT(YEAR FROM test_date),"-", EXTRACT(MONTH FROM test_date), "-",
+      EXTRACT(WEEK FROM test_date)) AS ymw,
+    counties.geo_id AS dl_geo_id
   FROM
     `measurement-lab.library.ndt_unified_downloads` tests
     JOIN counties ON ST_WITHIN(
@@ -26,14 +23,14 @@ mlab_dl AS (
     )
   WHERE
     client.Geo.country_name = "United States"
-    AND test_date >= '2020-01-01'
+    AND test_date >= '2019-12-29'
 ),
 mlab_ul AS (
   SELECT
     tests.*,
-    counties.geo_id AS geo_id,
-    counties.name AS county_name,
-    counties.WKT AS WKT
+    CONCAT(EXTRACT(YEAR FROM test_date),"-", EXTRACT(MONTH FROM test_date), "-",
+      EXTRACT(WEEK FROM test_date)) AS ymw,
+    counties.geo_id AS ul_geo_id
   FROM
     `mlab-sandbox.library.ndt_unified_uploads` tests
     JOIN counties ON ST_WITHIN(
@@ -45,16 +42,13 @@ mlab_ul AS (
     )
   WHERE
     client.Geo.country_name = "United States"
-    AND test_date >= '2020-01-01'
+    AND test_date >= '2019-12-29'
 
 ),
 dl_agg AS (
   SELECT
-  mlab_dl.geo_id,
-  county_name,
-  EXTRACT(YEAR FROM test_date) AS year,
-  EXTRACT(MONTH FROM test_date) AS month,
-  EXTRACT(WEEK FROM test_date) AS week,
+  mlab_dl.dl_geo_id AS dl_geo_id,
+  ymw AS dl_ymw,
   MIN(a.MeanThroughputMbps) AS MIN_download_Mbps,
   APPROX_QUANTILES(a.MeanThroughputMbps, 101) [SAFE_ORDINAL(26)] AS LOWER_QUARTILE_download_Mbps,
   APPROX_QUANTILES(a.MeanThroughputMbps, 101) [SAFE_ORDINAL(51)] AS MED_download_Mbps,
@@ -62,16 +56,13 @@ dl_agg AS (
   MAX(a.MeanThroughputMbps) AS MAX_download_Mbps,  
   AVG(a.MeanThroughputMbps) AS AVG_download_Mbps,
   APPROX_QUANTILES(CAST(a.MinRTT AS FLOAT64), 101) [ORDINAL(51)] as MED_min_rtt
-  FROM mlab_dl JOIN counties ON mlab_dl.county_name = counties.name
-  GROUP BY geo_id, county_name, year, month, week
+  FROM mlab_dl
+  GROUP BY dl_geo_id, dl_ymw
 ),
 ul_agg AS (
   SELECT
-  mlab_ul.geo_id,
-  county_name,
-  EXTRACT(YEAR FROM test_date) AS year,
-  EXTRACT(MONTH FROM test_date) AS month,
-  EXTRACT(WEEK FROM test_date) AS week,
+  mlab_ul.ul_geo_id AS ul_geo_id,
+  ymw,
   MIN(a.MeanThroughputMbps) AS MIN_upload_Mbps,
   APPROX_QUANTILES(a.MeanThroughputMbps,101) [SAFE_ORDINAL(26)] AS LOWER_QUARTILE_upload_Mbps,
   APPROX_QUANTILES(a.MeanThroughputMbps,101) [SAFE_ORDINAL(51)] AS MED_upload_Mbps,
@@ -79,15 +70,16 @@ ul_agg AS (
   MAX(a.MeanThroughputMbps) AS MAX_upload_Mbps,   
   AVG(a.MeanThroughputMbps) 
     AS AVG_upload_Mbps
-  FROM mlab_ul JOIN counties ON mlab_ul.county_name = counties.name
-  GROUP BY geo_id, county_name, year, month, week
+  FROM mlab_ul
+  GROUP BY ul_geo_id, ymw
 ),
 summary AS (
   SELECT
-    geo_id AS county_geoid, county_name, year, month, week, MIN_download_Mbps, LOWER_QUARTILE_download_Mbps, MED_download_Mbps,
+    dl_geo_id, ymw, MIN_download_Mbps, LOWER_QUARTILE_download_Mbps, MED_download_Mbps,
     UPPER_QUARTILE_download_Mbps, MAX_download_Mbps, AVG_download_Mbps, MED_min_rtt, MIN_upload_Mbps, LOWER_QUARTILE_upload_Mbps, 
     MED_upload_Mbps, UPPER_QUARTILE_upload_Mbps, MAX_upload_Mbps, AVG_upload_Mbps
   FROM
-  dl_agg JOIN ul_agg USING (geo_id, county_name, year, month, week)
+  dl_agg JOIN ul_agg ON dl_geo_id = ul_agg.ul_geo_id
+  JOIN counties ON dl_geo_id = counties.geo_id
 )
-SELECT * FROM summary JOIN counties ON summary.county_geoid = counties.geo_id
+SELECT * FROM summary
--- queries/us_zipcode.sql
+++ queries/us_zipcode.sql
@@ -1,5 +1,8 @@
 #standardSQL
-WITH dl AS (
+WITH zipcodes AS (
+  SELECT * FROM `bigquery-public-data.geo_us_boundaries.zip_codes`
+),
+dl AS (
   SELECT
     COUNT(a.UUID) AS ml_dl_count_tests,
     COUNT(DISTINCT client.IP) as ml_dl_count_ips,
@@ -8,22 +11,22 @@ WITH dl AS (
     EXTRACT(YEAR FROM test_date) AS year,
     EXTRACT(MONTH FROM test_date) AS month,
     EXTRACT(WEEK FROM test_date) AS week,
-    client.Geo.postal_code AS zip_code
+    zip_codes.zip_code AS zip
   FROM
     `measurement-lab.library.ndt_unified_downloads` tests,
     `bigquery-public-data.geo_us_boundaries.zip_codes` zip_codes
   WHERE
     client.Geo.country_name = "United States"
-    AND partition_date >= "2020-01-01"
+    AND test_date >= "2020-01-01"
     AND ST_WITHIN(
       ST_GeogPoint(
         client.Geo.longitude,
         client.Geo.latitude
       ),
-      zip_codes.zcta_geom
+      zip_codes.zip_code_geom
     )
   GROUP BY
-    zip_code,
+    zip,
     year,
     month,
     week
@@ -36,262 +39,39 @@ ul AS (
     EXTRACT(YEAR FROM test_date) AS year,
     EXTRACT(MONTH FROM test_date) AS month,
     EXTRACT(WEEK FROM test_date) AS week,
-    client.Geo.postal_code AS zip_code
+    zip_codes.zip_code AS zip
   FROM
-    `measurement-lab.release.ndt_uploads` tests,
+    `mlab-sandbox.library.ndt_unified_uploads` tests,
     `bigquery-public-data.geo_us_boundaries.zip_codes` zip_codes
   WHERE
-    connection_spec.server_geolocation.country_name = "United States"
-    AND partition_date >= "2020-01-01"
+    client.Geo.country_name = "United States"
+    AND test_date >= "2020-01-01"
     AND ST_WITHIN(
       ST_GeogPoint(
         client.Geo.longitude,
         client.Geo.latitude
       ),
-      zip_codes.zcta_geom
+      zip_codes.zip_code_geom
     )
   GROUP BY
-    zip_code,
+    zip,
     year,
     month,
     week
-),
-main AS (
-  SELECT
-    zip_code,
-    year,
-    month,
-    week,
-    ml_ul_count_tests,
-    ml_ul_count_ips,
-    ml_upload_Mbps,
-    ml_dl_count_tests,
-    ml_dl_count_ips,
-    ml_download_Mbps,
-    ml_min_rtt
-  FROM
-    dl
-    JOIN ul USING (zip_code, year, month, week)
-),
-main_dec_2014 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_dec_2014,
-    ml_ul_count_ips AS ml_ul_count_ips_dec_2014,
-    ml_upload_Mbps AS ml_upload_Mbps_dec_2014,
-    ml_dl_count_tests AS ml_dl_count_tests_dec_2014,
-    ml_dl_count_ips AS ml_dl_count_ips_dec_2014,
-    ml_download_Mbps AS ml_download_Mbps_dec_2014,
-    ml_min_rtt AS ml_min_rtt_dec_2014
-  FROM
-    main
-  WHERE
-    time_period = 'dec_2014'
-),
-main_jun_2015 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_jun_2015,
-    ml_ul_count_ips AS ml_ul_count_ips_jun_2015,
-    ml_upload_Mbps AS ml_upload_Mbps_jun_2015,
-    ml_dl_count_tests AS ml_dl_count_tests_jun_2015,
-    ml_dl_count_ips AS ml_dl_count_ips_jun_2015,
-    ml_download_Mbps AS ml_download_Mbps_jun_2015,
-    ml_min_rtt AS ml_min_rtt_jun_2015
-  FROM
-    main
-  WHERE
-    time_period = 'jun_2015'
-),
-main_dec_2015 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_dec_2015,
-    ml_ul_count_ips AS ml_ul_count_ips_dec_2015,
-    ml_upload_Mbps AS ml_upload_Mbps_dec_2015,
-    ml_dl_count_tests AS ml_dl_count_tests_dec_2015,
-    ml_dl_count_ips AS ml_dl_count_ips_dec_2015,
-    ml_download_Mbps AS ml_download_Mbps_dec_2015,
-    ml_min_rtt AS ml_min_rtt_dec_2015
-  FROM
-    main
-  WHERE
-    time_period = 'dec_2015'
-),
-main_jun_2016 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_jun_2016,
-    ml_ul_count_ips AS ml_ul_count_ips_jun_2016,
-    ml_upload_Mbps AS ml_upload_Mbps_jun_2016,
-    ml_dl_count_tests AS ml_dl_count_tests_jun_2016,
-    ml_dl_count_ips AS ml_dl_count_ips_jun_2016,
-    ml_download_Mbps AS ml_download_Mbps_jun_2016,
-    ml_min_rtt AS ml_min_rtt_jun_2016
-  FROM
-    main
-  WHERE
-    time_period = 'jun_2016'
-),
-main_dec_2016 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_dec_2016,
-    ml_ul_count_ips AS ml_ul_count_ips_dec_2016,
-    ml_upload_Mbps AS ml_upload_Mbps_dec_2016,
-    ml_dl_count_tests AS ml_dl_count_tests_dec_2016,
-    ml_dl_count_ips AS ml_dl_count_ips_dec_2016,
-    ml_download_Mbps AS ml_download_Mbps_dec_2016,
-    ml_min_rtt AS ml_min_rtt_dec_2016
-  FROM
-    main
-  WHERE
-    time_period = 'dec_2016'
-),
-main_jun_2017 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_jun_2017,
-    ml_ul_count_ips AS ml_ul_count_ips_jun_2017,
-    ml_upload_Mbps AS ml_upload_Mbps_jun_2017,
-    ml_dl_count_tests AS ml_dl_count_tests_jun_2017,
-    ml_dl_count_ips AS ml_dl_count_ips_jun_2017,
-    ml_download_Mbps AS ml_download_Mbps_jun_2017,
-    ml_min_rtt AS ml_min_rtt_jun_2017
-  FROM
-    main
-  WHERE
-    time_period = 'jun_2017'
-),
-main_dec_2017 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_dec_2017,
-    ml_ul_count_ips AS ml_ul_count_ips_dec_2017,
-    ml_upload_Mbps AS ml_upload_Mbps_dec_2017,
-    ml_dl_count_tests AS ml_dl_count_tests_dec_2017,
-    ml_dl_count_ips AS ml_dl_count_ips_dec_2017,
-    ml_download_Mbps AS ml_download_Mbps_dec_2017,
-    ml_min_rtt AS ml_min_rtt_dec_2017
-  FROM
-    main
-  WHERE
-    time_period = 'dec_2017'
-),
-main_jun_2018 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_jun_2018,
-    ml_ul_count_ips AS ml_ul_count_ips_jun_2018,
-    ml_upload_Mbps AS ml_upload_Mbps_jun_2018,
-    ml_dl_count_tests AS ml_dl_count_tests_jun_2018,
-    ml_dl_count_ips AS ml_dl_count_ips_jun_2018,
-    ml_download_Mbps AS ml_download_Mbps_jun_2018,
-    ml_min_rtt AS ml_min_rtt_jun_2018
-  FROM
-    main
-  WHERE
-    time_period = 'jun_2018'
-),
-main_dec_2018 AS (
-  SELECT
-    zip_code,
-    time_period,
-    ml_ul_count_tests AS ml_ul_count_tests_dec_2018,
-    ml_ul_count_ips AS ml_ul_count_ips_dec_2018,
-    ml_upload_Mbps AS ml_upload_Mbps_dec_2018,
-    ml_dl_count_tests AS ml_dl_count_tests_dec_2018,
-    ml_dl_count_ips AS ml_dl_count_ips_dec_2018,
-    ml_download_Mbps AS ml_download_Mbps_dec_2018,
-    ml_min_rtt AS ml_min_rtt_dec_2018
-  FROM
-    main
-  WHERE
-    time_period = 'dec_2018'
 )
 SELECT
-  ml_ul_count_tests_dec_2014,
-  ml_ul_count_ips_dec_2014,
-  ml_upload_Mbps_dec_2014,
-  ml_dl_count_tests_dec_2014,
-  ml_dl_count_ips_dec_2014,
-  ml_download_Mbps_dec_2014,
-  ml_min_rtt_dec_2014,
-  ml_ul_count_tests_jun_2015,
-  ml_ul_count_ips_jun_2015,
-  ml_upload_Mbps_jun_2015,
-  ml_dl_count_tests_jun_2015,
-  ml_dl_count_ips_jun_2015,
-  ml_download_Mbps_jun_2015,
-  ml_min_rtt_jun_2015,
-  ml_ul_count_tests_dec_2015,
-  ml_ul_count_ips_dec_2015,
-  ml_upload_Mbps_dec_2015,
-  ml_dl_count_tests_dec_2015,
-  ml_dl_count_ips_dec_2015,
-  ml_download_Mbps_dec_2015,
-  ml_min_rtt_dec_2015,
-  ml_ul_count_tests_jun_2016,
-  ml_ul_count_ips_jun_2016,
-  ml_upload_Mbps_jun_2016,
-  ml_dl_count_tests_jun_2016,
-  ml_dl_count_ips_jun_2016,
-  ml_download_Mbps_jun_2016,
-  ml_min_rtt_jun_2016,
-  ml_ul_count_tests_dec_2016,
-  ml_ul_count_ips_dec_2016,
-  ml_upload_Mbps_dec_2016,
-  ml_dl_count_tests_dec_2016,
-  ml_dl_count_ips_dec_2016,
-  ml_download_Mbps_dec_2016,
-  ml_min_rtt_dec_2016,
-  ml_ul_count_tests_jun_2017,
-  ml_ul_count_ips_jun_2017,
-  ml_upload_Mbps_jun_2017,
-  ml_dl_count_tests_jun_2017,
-  ml_dl_count_ips_jun_2017,
-  ml_download_Mbps_jun_2017,
-  ml_min_rtt_jun_2017,
-  ml_ul_count_tests_dec_2017,
-  ml_ul_count_ips_dec_2017,
-  ml_upload_Mbps_dec_2017,
-  ml_dl_count_tests_dec_2017,
-  ml_dl_count_ips_dec_2017,
-  ml_download_Mbps_dec_2017,
-  ml_min_rtt_dec_2017,
-  ml_ul_count_tests_jun_2018,
-  ml_ul_count_ips_jun_2018,
-  ml_upload_Mbps_jun_2018,
-  ml_dl_count_tests_jun_2018,
-  ml_dl_count_ips_jun_2018,
-  ml_download_Mbps_jun_2018,
-  ml_min_rtt_jun_2018,
-  ml_ul_count_tests_dec_2018,
-  ml_ul_count_ips_dec_2018,
-  ml_upload_Mbps_dec_2018,
-  ml_dl_count_tests_dec_2018,
-  ml_dl_count_ips_dec_2018,
-  ml_download_Mbps_dec_2018,
-  ml_min_rtt_dec_2018,
-  zip_codes.zip_code AS zip_code,
-  zip_codes.county AS county_name,
-  zip_codes.zcta_geom AS WKT
+  zip,
+  year,
+  month,
+  week,
+  ml_ul_count_tests,
+  ml_ul_count_ips,
+  ml_upload_Mbps,
+  ml_dl_count_tests,
+  ml_dl_count_ips,
+  ml_download_Mbps,
+  ml_min_rtt
 FROM
-  `mlab-sandbox.usa_geo.us_zip_codes` zip_codes
-  LEFT JOIN main_dec_2014 USING (zip_code)
-  LEFT JOIN main_jun_2015 USING (zip_code)
-  LEFT JOIN main_dec_2015 USING (zip_code)
-  LEFT JOIN main_jun_2016 USING (zip_code)
-  LEFT JOIN main_dec_2016 USING (zip_code)
-  LEFT JOIN main_jun_2017 USING (zip_code)
-  LEFT JOIN main_dec_2017 USING (zip_code)
-  LEFT JOIN main_jun_2018 USING (zip_code)
-  LEFT JOIN main_dec_2018 USING (zip_code);
+  dl
+  JOIN ul USING (zip, year, month, week)
+  JOIN zipcodes ON (dl.zip = zipcodes.zip_code)
