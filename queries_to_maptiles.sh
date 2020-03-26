302aaa38e0a4716d0195bc346431c71ba234fae2
Chris Ritzo <critzo@measurementlab.net>
2020-03-26 13:03:27 -0400

    location of csvs changed, title of us_counties template updated

--- queries_to_maptiles.sh
+++ queries_to_maptiles.sh
@@ -6,7 +6,6 @@ PROJECT="${1:?Please provide a GCP project for tile upload}"
 USERNAME="critzo"
 TABLE="${USERNAME}.temp"
 QUALIFIED_TABLE="${PROJECT}:${TABLE}"
-PUB_LOC="maptiles.mlab-sandbox.measurementlab.net"
 
 declare -a query_jobs=("us_counties")
 
@@ -17,13 +16,11 @@ for val in ${query_jobs[@]}; do
   GCS_STORAGE="${RESULT_NAME}_temp"
   PUB_LOC="maptiles.mlab-sandbox.measurementlab.net/${RESULT_NAME}"
 
-
   # TODO: replace use of bq with a custom Go binary that runs the query and
   # outputs the right format. Also open an HTTP port for Cloud Run.
 
   # Make a temporary table using a schema definition based on the
   # expected query output fields.
-
   bq mk --table --expiration 3600 --description "temp table to store intermediate results before automated export" \
     "${QUALIFIED_TABLE}" "schemas/${SCHEMA}"
 
@@ -54,10 +51,6 @@ for val in ${query_jobs[@]}; do
   # Fetch the CSV files that were just exported.
   gsutil -m cp gs://${GCS_STORAGE}/${RESULT_NAME}_*.csv ./csv/
 
-  # Cleanup the files on GCS because we don't need them there anymore.
-  gsutil rm -r gs://${GCS_STORAGE}
-  bq rm -f ${QUALIFIED_TABLE}
-
   # ogr2ogr requires a schema file to know which csv column represents
   # the geometry. We pass all filenames to the inference script, but
   # it only reads the first one, since the schema should be consistent
@@ -67,7 +60,7 @@ for val in ${query_jobs[@]}; do
   # Use xargs to convert all the csv files to geojson individually, in
   # parallel. We will aggregate them in the next step.  See csv_to_geojson
   # script for ogr2ogr args.
-  echo ${RESULT_NAME}_*.csv | xargs -n1 -P4 scripts/csv_to_geojson.sh 
+  echo csv/${RESULT_NAME}_*.csv | xargs -n1 -P4 scripts/csv_to_geojson.sh 
 
   # Let tippecanoe read all the geojson files into one layer.
   tippecanoe -e ./maptiles/${RESULT_NAME} -f -l ${RESULT_NAME} *.geojson -z6 \
@@ -87,6 +80,12 @@ for val in ${query_jobs[@]}; do
     cp -r ./templates/us_counties.html gs://${PUB_LOC}/index.html
 
   # Final cleanup
+
+  # Cleanup the files on GCS because we don't need them there anymore.
+  gsutil rm -r gs://${GCS_STORAGE}
+  bq rm -f ${QUALIFIED_TABLE}
+
+  # Cleanup locally generated files
   rm *.csvt *.geojson csv/* maptiles/*
 
   # NOTE: if the html and tiles are served from different domains we'll need to
