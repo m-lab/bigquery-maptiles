3d57700672a185ca7da9711a93578759059cdacb
Chris Ritzo <critzo@measurementlab.net>
2020-06-22 14:27:51 -0400

    debugging

--- queries/export_continent_country_region_stats.sql
+++ queries/export_continent_country_region_stats.sql
@@ -1,4 +1,4 @@
 SELECT * FROM `measurement-lab.mlab_statistics.continent_country_region_maxDL_histogram`
-WHERE continent_code = '@continent_code'
-AND country_code = '@country_code'
-AND ISO3166_2region1 = '@country_code-@region_code'
+WHERE continent_code = "@continent_code"
+AND country_code = "@country_code"
+AND ISO3166_2region1 = "@country_code-@region_code"
