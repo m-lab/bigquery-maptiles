917dd417ee340e66a45d5b1b1d71680b285667fd
Chris Ritzo <critzo@measurementlab.net>
2020-07-07 16:06:03 -0400

    moved files

--- queries/export_continent_country_region_stats.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM `measurement-lab.mlab_statistics.continent_country_region_maxDL_histogram`
-WHERE continent_code = @continent_code
-AND country_code = @country_code
-AND ISO3166_2region1 = CONCAT(@country_code, "-", @region_code)
